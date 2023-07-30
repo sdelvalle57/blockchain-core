766df4eb41a739d45c7857d8ed21ee1673b2a249
sdelvalle57 <sdelvalle57@gmail.com>
2023-07-30 15:56:38 -0500

    still trying to verify the message

--- src/server/rpc_call.rs
+++ src/server/rpc_call.rs
@@ -1,16 +1,14 @@
-use std::io;
-use web3::signing::{keccak256, recover};
-use std::{vec, result, io::Error};
-use serde_json;
 use serde::{Deserialize, Serialize};
+use serde_json;
+use std::io;
+use std::{io::Error, result, vec};
+use web3::signing::{keccak256, recover};
 
-use juniper::{
-    GraphQLEnum, GraphQLInputObject, GraphQLObject, FieldError
-};
+use juniper::{FieldError, GraphQLEnum, GraphQLInputObject, GraphQLObject};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Accounts {
-    data: Vec<String>
+    data: Vec<String>,
 }
 
 #[derive(GraphQLInputObject)]
@@ -23,14 +21,13 @@ pub struct RpcCall {
 }
 
 impl RpcCall {
-
-    fn message_to_vec(&self) -> result::Result<Vec<String>, FieldError>  {
+    fn message_to_vec(&self) -> result::Result<Vec<String>, FieldError> {
         let v: Accounts = serde_json::from_str(&self.message)?;
         println!("{:?}", v);
         Ok(v.data)
     }
 
-    fn validate_signature(&self) -> Result<(), io::Error>  {
+    fn validate_signature(&self) -> Result<(), io::Error> {
         let eth_message = keccak256(
             format!(
                 "{}{}{}",
@@ -43,20 +40,27 @@ impl RpcCall {
         println!("{}", self.signature);
         let signature = hex::decode(&self.signature);
         match signature {
-            Err(_) => return Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid Signature Format")),
-            _ => ()
+            Err(_) => {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    "Invalid Signature Format",
+                ))
+            }
+            _ => (),
         };
 
-        //TODO: message validation is not working, probably message needs to be arrayfy before recovering 
+        //TODO: message validation is not working, probably message needs to be arrayfy before recovering
         let pub_key = recover(&eth_message, &signature.unwrap()[..64], 0).unwrap();
         let pub_key = format!("{:02X?}", pub_key);
         if pub_key == self.sender {
-            return Ok(())
+            return Ok(());
         }
-        Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid Signature"))
+        Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            "Invalid Signature",
+        ))
     }
 }
-
 
 impl TryInto<BlockchainInitiated> for RpcCall {
     type Error = FieldError;
@@ -66,7 +70,7 @@ impl TryInto<BlockchainInitiated> for RpcCall {
         //     values: vec::Vec::new(),
         //     chain: ChainName::MAINNET,
         //     chain_id: self.chain_id
-        // }) 
+        // })
         match self.message_to_vec() {
             Ok(accounts) => {
                 let chain = self.chain_id.try_into()?;
@@ -76,8 +80,8 @@ impl TryInto<BlockchainInitiated> for RpcCall {
                     accounts,
                     values: vec::Vec::new(),
                     chain,
-                    chain_id: self.chain_id
-                }) 
+                    chain_id: self.chain_id,
+                })
             }
             Err(error) => {
                 println!("{:?}", error);
@@ -90,10 +94,10 @@ impl TryInto<BlockchainInitiated> for RpcCall {
 #[derive(GraphQLEnum)]
 #[graphql(description = "Types of blockchains")]
 enum ChainName {
-    MAINNET = 1,   
+    MAINNET = 1,
 
     #[allow(non_camel_case_types)]
-    MATIC_MAINNET = 137
+    MATIC_MAINNET = 137,
 }
 
 impl TryFrom<i32> for ChainName {
@@ -102,7 +106,10 @@ impl TryFrom<i32> for ChainName {
         match value {
             1 => Ok(ChainName::MAINNET),
             137 => Ok(ChainName::MATIC_MAINNET),
-            _ => Err(Error::new(std::io::ErrorKind::InvalidInput, format!("Invalid Chain, use chain ids {} or {}", 1, 2)))
+            _ => Err(Error::new(
+                std::io::ErrorKind::InvalidInput,
+                format!("Invalid Chain, use chain ids {} or {}", 1, 2),
+            )),
         }
     }
 }
@@ -116,30 +123,37 @@ impl ToString for ChainName {
     }
 }
 
-
 #[derive(GraphQLObject)]
 #[graphql(description = "Response for a successful init blovkchain request")]
 pub struct BlockchainInitiated {
     accounts: Vec<String>,
     values: Vec<String>,
     chain: ChainName,
-    chain_id: i32
+    chain_id: i32,
 }
 
-
-
+pub fn eth_message(message: String) -> [u8; 32] {
+    keccak256(
+        format!(
+            "{}{}{}",
+            "\x19Ethereum Signed Message:\n",
+            message.len(),
+            message
+        )
+        .as_bytes(),
+    )
+}
 
 #[test]
 fn test_recover() {
-    let message: RpcCall = {
-        RpcCall { 
-            message: "0x63f9a92d8d61b48a9fff8d58080425a3012d05c8igwyk4r1o7o".to_string(), 
-            signature: "382a3e04daf88f322730f6a2972475fc5646ea8c4a7f3b5e83a90b10ba08a7364cd2f55348f2b6d210fbed7fc485abf19ecb2f3967e410d6349dd7dd1d4487751b".to_string(),
-            sender: "0x63f9a92d8d61b48a9fff8d58080425a3012d05c8".to_string(), 
-            chain_id: 1
-        }
-    };
-    let res = message.validate_signature().unwrap();
-    assert_eq!(res, ());
-    
+    let account = "0x23C6599aAdF44Be7cbaD6D9051bb4C2255b2f713".to_string();
+    let message = "heelo".to_string();
+    let message = eth_message(message);
+    let signature = hex::decode("7dabc0471d53ed34ec21c0257d8b40f7234d2bce8ccdbfba540a8f2be56183a954e49162be4768c14efeb6ad7f1a836d2a3385df628ede34f7047af65f18cc621c").unwrap();
+    println!("{} {:?} {:?}", account, message, signature);
+    let pubkey = recover(&message, &signature[..64], 0);
+    assert!(pubkey.is_ok());
+    let pubkey = pubkey.unwrap();
+    let pubkey = format!("{:02X?}", pubkey);
+    assert_eq!(account, pubkey)
 }
