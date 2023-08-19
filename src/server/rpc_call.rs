34198cf5c0e3632c3a76258a1b080f6a226c31da
sdelvalle57 <sdelvalle57@gmail.com>
2023-08-19 16:38:43 -0500

    metamask messag verification

--- src/server/rpc_call.rs
+++ src/server/rpc_call.rs
@@ -29,16 +29,8 @@ impl RpcCall {
     }
 
     fn validate_signature(&self) -> Result<(), io::Error> {
-        let eth_message = keccak256(
-            format!(
-                "{}{}{}",
-                "\x19Ethereum Signed Message:\n",
-                self.message.len(),
-                self.message
-            )
-            .as_bytes(),
-        );
-        println!("{}", self.signature);
+        let eth_message = eth_message(self.message.to_string());
+        println!("signature: {}", self.signature);
         let signature = hex::decode(&self.signature);
         match signature {
             Err(_) => {
@@ -49,11 +41,13 @@ impl RpcCall {
             }
             _ => (),
         };
+        let signature = signature.unwrap();
 
         //TODO: message validation is not working, probably message needs to be arrayfy before recovering
-        let pub_key = recover(&eth_message, &signature.unwrap()[..64], 0).unwrap();
+        let recovery_id = signature[64] as i32 - 27;
+        let pub_key = recover(&eth_message, &signature[..64], recovery_id).unwrap();
         let pub_key = format!("{:02X?}", pub_key);
-        if pub_key == self.sender {
+        if pub_key.to_lowercase() == self.sender.to_lowercase() {
             return Ok(());
         }
         Err(io::Error::new(
@@ -145,33 +139,43 @@ pub fn eth_message(message: String) -> [u8; 32] {
     )
 }
 
-#[test]
-fn test_recover() {
-    let secret = "8da9966c46aee0993660a90ad6339c361c85301943ea5048770cd37787daa383";
-    // print!("{:?}", secret.as_bytes());
-    // let ethereum_secret_key = "0x...".to_string();
-    let sk = SecretKey::from_str(secret).unwrap();
-    assert_eq!(
-        sk.display_secret().to_string(),
-        "8da9966c46aee0993660a90ad6339c361c85301943ea5048770cd37787daa383"
-    );
+
+// #[test]
+// fn test_recover() {
+//     let secret = "8da9966c46aee0993660a90ad6339c361c85301943ea5048770cd37787daa383";
+//     // print!("{:?}", secret.as_bytes());
+//     // let ethereum_secret_key = "0x...".to_string();
+//     let sk = SecretKey::from_str(secret).unwrap();
+//     assert_eq!(
+//         sk.display_secret().to_string(),
+//         "8da9966c46aee0993660a90ad6339c361c85301943ea5048770cd37787daa383"
+//     );
 
     
+#[test]
+fn test_recover() {
+    let account = "0x23C6599aAdF44Be7cbaD6D9051bb4C2255b2f713".to_string();
+    let message = "heelo".to_string();
+    let message = eth_message(message);
+    let signature = hex::decode("7dabc0471d53ed34ec21c0257d8b40f7234d2bce8ccdbfba540a8f2be56183a954e49162be4768c14efeb6ad7f1a836d2a3385df628ede34f7047af65f18cc621c").unwrap();
+    println!("{} {:?} {:?}", account, message, signature);
+    let recovery_id = signature[64] as i32 - 27;
+    let pubkey = recover(&message, &signature[..64], recovery_id);
+    assert!(pubkey.is_ok());
+    let pubkey = pubkey.unwrap();
+    let pubkey = format!("{:02X?}", pubkey);
+    assert_eq!(account.to_lowercase(), pubkey.to_lowercase())
+}
 
+#[test]
+fn test_recover_rpc() {
+    let rpc_call: RpcCall = RpcCall { 
+        sender: "0x23C6599aAdF44Be7cbaD6D9051bb4C2255b2f713".to_string(), 
+        message: "heelo".to_string(), 
+        chain_id: 137, 
+        signature: "7dabc0471d53ed34ec21c0257d8b40f7234d2bce8ccdbfba540a8f2be56183a954e49162be4768c14efeb6ad7f1a836d2a3385df628ede34f7047af65f18cc621c".to_string() 
+    };
+    rpc_call.validate_signature().unwrap();
 
-
-
-    // assert_eq!(secret, secret_key.display_secret())
-    // secret_key.display_secret().to_string();
-
-    // let account = "0x23C6599aAdF44Be7cbaD6D9051bb4C2255b2f713".to_string();
-    // let message = "heelo".to_string();
-    // let message = eth_message(message);
-    // let signature = hex::decode("7dabc0471d53ed34ec21c0257d8b40f7234d2bce8ccdbfba540a8f2be56183a954e49162be4768c14efeb6ad7f1a836d2a3385df628ede34f7047af65f18cc621c").unwrap();
-    // println!("{} {:?} {:?}", account, message, signature);
-    // let pubkey = recover(&message, &signature[..64], 0);
-    // assert!(pubkey.is_ok());
-    // let pubkey = pubkey.unwrap();
-    // let pubkey = format!("{:02X?}", pubkey);
-    // assert_eq!(account, pubkey)
+    
 }
